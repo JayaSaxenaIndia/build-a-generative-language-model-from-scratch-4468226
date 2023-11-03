448d3e2060b0f66b84ec2c0cc3168642b0d594d5
JayaSaxenaIndia <147504254+JayaSaxenaIndia@users.noreply.github.com>
2023-11-03 17:20:57 +0000

    Added Similar words function

--- 03/03_02_end.py
+++ 03/03_02_end.py
@@ -12,9 +12,17 @@ with open(CURRENT_DIR + "/word_to_vector_trsf.pkl", "rb") as pk:
 
 def cosine_similarity(vec_a, vec_b):
     numerator = sum([vec_a[i] * vec_b[i] for i in range(len(vec_a))])
-    denominator = (norm(vec_a) * norm(vec_b))
+    denominator = (sum([vec_a[i] ** 2 for i in range(len(vec_a))]) ** 0.5 * sum([vec_b[i] ** 2 for i in range(len(vec_b))]) ** 0.5)
     return numerator / denominator
 
 print(cosine_similarity(word_to_vector["plant"], word_to_vector["grow"]))
 print(cosine_similarity(word_to_vector["minute"], word_to_vector["plant"]))
 print(cosine_similarity(word_to_vector["plant"], word_to_vector["tree"]))
+
+def similar_words(word="tree", top_k=10):
+    return sorted(
+        word_to_vector.keys(), 
+        key=lambda x: -cosine_similarity(word_to_vector[word], word_to_vector[x])
+    )[:top_k]
+    
+result = Answer.similar_words("tree", top_k=10)
